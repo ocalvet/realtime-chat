616cc11bbc4074ad775086c00abcee9e3f1a3ab8
Ovidio R. Calvet <calveto@gmail.com>
2018-05-19 21:49:17 -0400

    connect client with server sending messages

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('disconnect', function(e){
+    console.log('user disconnected', e);
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
