7b2ed2604554baf8072059372e8818b220492598
Ovidio R. Calvet <calveto@gmail.com>
2018-05-20 21:34:27 -0400

    remove js code from index.html

--- client/app.js
+++ client/app.js
@@ -1,9 +1,36 @@
+import ioClient from 'socket.io-client'
+
 class App {
   constructor() {
     console.log('App created');
+    this.io = ioClient('http://localhost:3000');
+    this.wireEvents();
+  }
+  wireEvents() {
+    this.io.on('chat message', (msg) => {
+      this.addMessage(msg);
+    });
+    this.io.on('user connected', (data) => {
+      this.addMessage('User connected');
+    })
   }
   init() {
     console.log('App initializing');
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const messageInput = document.querySelector('#m');
+      const msg = messageInput.value;
+      this.io.emit('chat message', msg);
+      this.addMessage(msg);
+      return false;
+    });
+  }
+  addMessage(msg) {
+    const messagesContainer = document.querySelector('#messages');
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = msg;
+    messagesContainer.appendChild(newMessage);
   }
 }
 
