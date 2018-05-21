e4cf8a14f603bad10da81ce0ceb9eb824a446399
Ovidio R. Calvet <calveto@gmail.com>
2018-05-20 21:08:21 -0400

    structure project better

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 app.use(express.static('dist'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '../dist/index.html');
 });
 
 io.on('connection', function(socket){
