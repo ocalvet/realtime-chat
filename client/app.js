5c59b9379ad4e95ba7f123530e8b21979d1a7b45
Ovidio R. Calvet <calveto@gmail.com>
2018-05-20 20:57:04 -0400

    add webpack support

---
+++ client/app.js
@@ -0,0 +1,10 @@
+class App {
+  constructor() {
+    console.log('App created');
+  }
+  init() {
+    console.log('App initializing');
+  }
+}
+
+export default App;
---
+++ client/main.js
@@ -0,0 +1,7 @@
+import App from './app';
+
+(function(document) {
+  console.log('We are looking good');
+  const app = new App();
+  app.init();
+})(document);
